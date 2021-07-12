c018825049897208369023d0e3d4ee1b1d0abc55
tonyfrenzy <tonyfrenzy@gmail.com>
2021-07-12 09:41:25 +0100

    add updateCart endpoint

--- src/controllers/CartController.js
+++ src/controllers/CartController.js
@@ -77,7 +77,63 @@ const CartController = {
   },
 
 
-  updateCart: async (req, res) => {},
+  updateCart: async (req, res) => {
+    const { id } = req.params;
+    let { userId, items } = req.body;
+
+    // simulate new item addition 
+    items = [
+      {
+          productId: '60eb817eda01791328cddc47',
+          name: 'shoes',
+          quantity: 4,
+          price: 103
+      },
+      {
+          productId: '60eb8197da01791328cddc4a',
+          name: 'Jewelry!',
+          quantity: 4,
+          price: 300
+      },
+    ];   
+    const newItem = {
+        productId: "60eb8160da01791328cddc44",
+        name: "Clothing!",
+        quantity: 5,
+        price: 250
+    };
+    items.push(newItem);
+
+    const billArray = items.map((i) => (i.quantity * i.price));
+    const bill = billArray.reduce((prevTotal, nextPrice) => prevTotal + nextPrice, 0);
+
+    if (!userId || !items) {
+      return res.status(400)
+        .json({ status: 'fail', message: 'cart may be empty' });
+    }
+
+    try {      
+      const cart = await Cart.findById(id).exec();
+
+      cart.items = items;
+      cart.bill = bill;
+      await cart.save();
+
+      if (!cart) {
+        return res
+          .status(400)
+          .json({ status: 'fail', message: 'cart not updated' });
+      }
+
+      return res.status(200)
+        .json({ status: 'success', message: 'cart updated successfully', data: cart });
+   
+    } catch (err) {
+      return res.status(500)
+        .json({ status: 'fail', message: 'server err', err 
+      });
+    }
+  },
 
 
   deleteCart: async (req, res) => {},

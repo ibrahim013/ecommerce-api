38c89b596ba6f1b13be1fef1e01bbc41f4f99fb6
tonyfrenzy <tonyfrenzy@gmail.com>
2021-07-20 18:33:00 +0100

    add basic test

---
+++ src/__test__/app.test.js
@@ -0,0 +1,16 @@
+import supertest from 'supertest';
+import app from '../server.js';
+
+const request = supertest(app);
+
+it('get route', async () => {
+  const response = await request.get('/');
+  expect(response.status).toBe(200);
+  expect(response.body.message).toBe('hello world');
+});
+
+it('doesnt get route', async () => {
+  const response = await request.get('/');
+  expect(response.status).toBe(200);
+  expect(response.body.message).not.toBe('hello');
+});
